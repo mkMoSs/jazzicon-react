d0e8fe319401e87210408db1d9115513c7ed786a
MoSs <mkmoss.gr@gmail.com>
2022-01-05 14:54:05 +0200

    remove surrounding div & apply styles to Paper

--- src/paper.component.tsx
+++ src/paper.component.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import Color from 'color';
 
 export const BASE_STYLES = {
-  borderRadius: '50%',
-  overflow: 'hidden',
-  padding: 0,
-  margin: 0,
-  width: '100%',
-  height: '100%',
-  display: 'inline-block',
+    borderRadius: '50%',
+    overflow: 'hidden',
+    padding: 0,
+    margin: 0,
+    width: '100%',
+    height: '100%',
+    display: 'inline-block',
 };
 
 export interface PaperProps {
-  color: Color;
-  className?: string;
+    color: Color;
+    className?: string;
 }
 
 export function Paper(props: React.PropsWithChildren<PaperProps>) {
-  const style = Object.assign({}, BASE_STYLES, {
-    backgroundColor: props.color.hex(),
-  });
+    const style = Object.assign({}, BASE_STYLES, {
+        backgroundColor: props.color.hex(),
+    });
 
-  return (
-    <div className={props.className}>
-      <div style={style}>{props.children}</div>
-    </div>
-  );
+    return (
+        <div className={props.className} style={{...props.style, ...style}}>
+            {props.children}
+        </div>
+    );
 }
